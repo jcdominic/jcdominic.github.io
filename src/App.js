a1171c671da6e6d524f51d159f7218f44b54dcd3
Justin Dominic <justincdominic@gmail.com>
2023-01-01 11:05:47 -0800

    Updated logo and redirects, also trying to set ssl to legacy

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ const App = () => (
     <Suspense fallback={<Main />}>
       <Switch>
         <Route exact path="/" component={About} />
-        <Route path="/about" component={About} />
-        {/* MIGHT BE NEEDED LATER--TBD */}
+        {/* <Route path="/about" component={About} /> */}
         <Route path="/projects" component={Projects} />
         {/* <Route path="/stats" component={Stats} /> */}
         <Route path="/contact" component={Contact} />
