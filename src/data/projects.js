4b1f339f6e1b6b599bc73cd8d778d6a981c74126
Justin Dominic <justincdominic@gmail.com>
2023-01-01 17:29:20 -0800

    Added old pong project with some new art

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,17 @@ const data = [
       + 'Implemented in Orca, '
       + 'an esoteric programming language and livecoding environment designed to quickly create procedural sequencers, in which every letter of the alphabet is an operation, where lowercase letters operate on bang, uppercase letters operate each frame.',
   },
+  {
+    title: 'Pong Game',
+    subtitle: 'A single-player pong game',
+    link: 'https://jcdominic.github.io/pong/',
+    image: '/images/projects/pong.png',
+    date: '2018-08',
+    desc:
+      'A single-player pong game. '
+      + 'Implemented in JavaScript, '
+      + 'with supporting code in HTML and CSS. ',
+  },
 ];
 
 export default data;
